99ae65c646af58242b0e7eb03204d350e57fc332
takadogg <takadogg66@gmail.com>
2024-05-28 17:51:33 +0900

    ログイン/ログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'には＠を含めてください' }
- 
-  validates :password, presence: true, length: { minimum: 6 }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-  validates_confirmation_of :password, message: 'とパスワード（確認）が一致しません'
   
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
@@ -19,8 +12,8 @@ class User < ApplicationRecord
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナ文字を使用してください' } do
-    validates :first_name
-    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
   end
 
   validates :birth_date, presence: true
