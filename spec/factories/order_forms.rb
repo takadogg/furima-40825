38ac410812336db8c0622287144ebde5f5a3d725
takadogg <takadogg66@gmail.com>
2024-06-06 10:34:30 +0900

    formオブジェクトパターン単体テスト

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_form do
+    postal_code      { '123-4567'}
+    prefecture_id    { Faker::Number.between(from: 2, to: 48) }  
+    city             { '横浜市緑区' }
+    address_line1    { '1-1' }
+    address_line2    { '青山ビル' }
+    phone_number     { '09012345678' }
+  end
+end
+ 
